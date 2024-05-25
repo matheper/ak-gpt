e495160bd93b8106d25d9bba9bb4797cdecb2d91
Matheus Pereira <matheper@gmail.com>
2024-05-25 15:12:06 -0400

    use logging

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import torch
+import logging
+from tokenizer import Tokenizer
 
 from dataset import (
     SimpleDataLoader,
@@ -6,7 +8,9 @@ from dataset import (
     load_hf_dataset,
     split_dataset,
 )
-from tokenizer import Tokenizer
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 def main():
@@ -18,7 +22,7 @@ def main():
         max_size=200,
     )
 
-    print(hf_dataset)
+    logging.info(hf_dataset)
 
     # concatenate and clean text leaving only latin characters
     dataset = concat_and_clean_text(hf_dataset)
@@ -26,28 +30,28 @@ def main():
     # train tokenizer with the entire dataset
     tokenizer = Tokenizer()
     tokenizer.train(dataset)
-    print(f"Vocabulary size: {tokenizer.vocabulary_size}")
-    print(f"Vocabulary mapping:\n{tokenizer.str_to_int}\n")
+    logging.info(f"Vocabulary size: {tokenizer.vocabulary_size}")
+    logging.info(f"Vocabulary mapping:\n{tokenizer.str_to_int}\n")
 
     # test tokenizer with a slice of the dataset
     text = dataset[:25]
-    print(f"Text: {text}")
+    logging.info(f"Text: {text}")
     tokens = tokenizer.tokenize(text)
-    print(f"Tokens: {tokens}")
+    logging.info(f"Tokens: {tokens}")
     encoded = tokenizer.encode(text)
-    print(f"Encoded: {encoded}")
+    logging.info(f"Encoded: {encoded}")
     decoded = tokenizer.decode(encoded)
-    print(f"Decoded: {decoded}")
+    logging.info(f"Decoded: {decoded}")
 
     # load the entire encoded dataset into a torch tensor
     data = torch.tensor(tokenizer.encode(dataset), dtype=torch.long)
-    print(f"Data shape: {data.shape}")
+    logging.info(f"Data shape: {data.shape}")
 
     # split dataset into train and validation
     split_ratio = 0.9
     train_data, val_data = split_dataset(data, split_ratio=split_ratio)
-    print(f"Train shape: {train_data.shape}")
-    print(f"Validation shape: {val_data.shape}")
+    logging.info(f"Train shape: {train_data.shape}")
+    logging.info(f"Validation shape: {val_data.shape}")
 
     train_gen = torch.Generator()
     train_gen.manual_seed(42)
@@ -70,9 +74,8 @@ def main():
     )
 
     x_batch, y_batch = train_dl.get_batch()
-    print(f"X batch: {x_batch}")
-    print(f"Y batch: {y_batch}")
-    
+    logging.info(f"X batch: {x_batch}")
+    logging.info(f"Y batch: {y_batch}")
 
 
 if __name__ == "__main__":
