01a1ab8ccd5564decdbe5aa0367a4203eee6d5de
Matheus Pereira <matheper@gmail.com>
2024-05-25 12:31:20 -0400

    main function

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from dataset import concat_clean_text, load_hf_dataset
 
 
-# load a slice of wikipedia pt
-hf_dataset = load_hf_dataset(
-    dataset="wikimedia/wikipedia",
-    subset="20231101.pt",
-    split="train",
-    max_size=200,
-)
+def main():
+    # load a slice of wikipedia pt
+    hf_dataset = load_hf_dataset(
+        dataset="wikimedia/wikipedia",
+        subset="20231101.pt",
+        split="train",
+        max_size=200,
+    )
 
-print(hf_dataset)
+    print(hf_dataset)
 
-# concatenate and clean text leaving only latin characters
-dataset = concat_clean_text(hf_dataset)
-print(sorted(list(set(dataset))))
+    # concatenate and clean text leaving only latin characters
+    dataset = concat_clean_text(hf_dataset)
+    print(sorted(list(set(dataset))))
+
+
+if __name__ == "__main__":
+    main()
