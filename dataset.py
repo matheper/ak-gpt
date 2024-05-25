a77e7b3b684422f5072d6c58cf515468be073762
Matheus Pereira <matheper@gmail.com>
2024-05-25 12:59:31 -0400

    dataset type hint

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import re
 from functools import partial
+from typing import Optional
 
 from datasets import Dataset, load_dataset
 
@@ -8,7 +9,12 @@ def _gen_from_iterable_dataset(iterable_ds):
     yield from iterable_ds
 
 
-def load_hf_dataset(dataset, subset=None, split=None, max_size=0):
+def load_hf_dataset(
+    dataset: str,
+    subset: Optional[str] = None,
+    split: Optional[str] = None,
+    max_size: int = 0,
+) -> Dataset:
     """Load a Hugging Face Dataset with the option to partially download it.
     https://stackoverflow.com/a/76662982/1704191
     """
@@ -37,7 +43,7 @@ def latin_clean_text(text: str) -> str:
     return cleaned_text
 
 
-def concat_clean_text(dataset: Dataset):
+def concat_clean_text(dataset: Dataset) -> str:
     """Concatenate and clean text from a Hugging Face Dataset."""
     concatenated_text = "\n".join([d["text"] for d in dataset])
     return latin_clean_text(concatenated_text)
