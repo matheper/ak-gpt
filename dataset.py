81badfff5b97f857680770d6c1f7f27af8eb0519
Matheus Pereira <matheper@gmail.com>
2024-05-25 12:29:39 -0400

    Dataset clean up

--- dataset.py
+++ dataset.py
@@ -1,23 +1,43 @@
+import re
 from functools import partial
 
 from datasets import Dataset, load_dataset
 
 
-# https://stackoverflow.com/a/76662982/1704191
-def gen_from_iterable_dataset(iterable_ds):
+def _gen_from_iterable_dataset(iterable_ds):
     yield from iterable_ds
 
 
 def load_hf_dataset(dataset, subset=None, split=None, max_size=0):
+    """Load a Hugging Face Dataset with the option to partially download it.
+    https://stackoverflow.com/a/76662982/1704191
+    """
     if max_size > 0:
         iter_ds = load_dataset(
             dataset, subset, split=split, streaming=True
         ).take(max_size)
         hf_dataset = Dataset.from_generator(
-            partial(gen_from_iterable_dataset, iter_ds),
+            partial(_gen_from_iterable_dataset, iter_ds),
             features=iter_ds.features,
         )
     else:
         hf_dataset = load_dataset(dataset, subset, split=split)
 
     return hf_dataset
+
+
+def latin_clean_text(text: str) -> str:
+    """Clean text by removing characters that are not in
+    the range from ' '(space) to 'ÿ' or are '\n' or '\t'.
+    Basic Latin + Latin-1 Supplement characters from Unicode.
+    https://en.wikipedia.org/wiki/List_of_Unicode_characters
+    """
+    pattern = r"[ -ÿ\t\n]+"
+    cleaned_text = "".join(re.findall(pattern, text))
+    return cleaned_text
+
+
+def concat_clean_text(dataset: Dataset):
+    """Concatenate and clean text from a Hugging Face Dataset."""
+    concatenated_text = "\n".join([d["text"] for d in dataset])
+    return latin_clean_text(concatenated_text)
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from dataset import load_hf_dataset
+from dataset import concat_clean_text, load_hf_dataset
 
 
-# https://huggingface.co/datasets/wikimedia/wikipedia
-dataset = load_hf_dataset(
+# load a slice of wikipedia pt
+hf_dataset = load_hf_dataset(
     dataset="wikimedia/wikipedia",
     subset="20231101.pt",
     split="train",
     max_size=200,
 )
 
-print(dataset)
-print(dataset[0])
+print(hf_dataset)
+
+# concatenate and clean text leaving only latin characters
+dataset = concat_clean_text(hf_dataset)
+print(sorted(list(set(dataset))))
